fc3760215ed255da50a5e842d6771c700e3a3e43
jgjorge96 <jgjorge96@gmail.com>
2023-06-29 17:51:33 -0300

    marking added items and high priority ready

--- src/App.vue
+++ src/App.vue
@@ -2,31 +2,56 @@
   import { ref } from 'vue';
 
   const header = ref ('Shopping List App')
+  const editing = ref(false)
   const items = ref([ 
-    {id: 1, label: "10 tomates"},
-    {id: 2, label: "5 chocolates"},
-    {id: 3, label: "2 gaseosas"},
+    {id: 1, label: "10 tomates", purchased: true, highPriority: false},
+    {id: 2, label: "5 chocolates", purchased: true, highPriority: false},
+    {id: 3, label: "2 gaseosas", purchased: false, highPriority: true},
   ])
 
   const newItem = ref("")
   const newItemHightPriority = ref("low")
+  const saveItems = () => {
+    items.value.push({id: items.value.length + 1, label: newItem.value, highPriority: newItemHightPriority.value})
+    newItem.value = ""
+    newItemHightPriority.value= ""
+  }
+  const doEdit = (e) => {
+    editing.value = e
+    newItem.value = ""
+    newItemHightPriority.value= ""
+  }
+  const togglePurchased = (item) => {
+    item.purchased= !item.purchased
+  }
 </script>
 
 <template>
-  <h1> {{ header }}</h1>
+  <div>
+    <h1> {{ header }}</h1>
+    <button v-if="editing" @click="doEdit(false)">Cancelar</button>
+    <button v-else @click="doEdit(true)">Añadir Producto</button>
+  </div>
 
-  <form @submit.prevent="items.push({id: items.length + 1, label: newItem})">
+  <form @submit.prevent="saveItems" v-if="editing">
     <input v-model.trim="newItem" type="text" placeholder="Añadir producto"> {{ newItem }}
     <label>
       <input type="checkbox" v-model="newItemHightPriority">Muy importante
     </label>
-    <button>Añadir a la lista</button>
+    <button :disabled="newItem.length < 2">Añadir a la lista</button>
   </form>
   <ul>
-    <li v-for="({id, label}, index) in items" :key="id">{{ label }}</li>
+    <li v-for="(item, index) in items" @click="togglePurchased(item)" :key="item.id" :class="{strikeout: item.purchased, priority: item.highPriority}">{{ item.label }}</li>
   </ul>
+  <p v-if="!items.length">Lista vacia</p>
 </template>
 
 <style lang="scss">
   @import './assets/styles.scss';
+  .strikeout {
+    color: blue;
+  }
+  .priority {
+    color: red;
+  }
 </style>
