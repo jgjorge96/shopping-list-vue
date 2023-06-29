03154bcfd2ddcf33d42d5ac0b89ab711e77f368d
jgjorge96 <jgjorge96@gmail.com>
2023-06-29 16:50:48 -0300

    adding items to list ready

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,30 @@
 <script setup>
+  import { ref } from 'vue';
 
+  const header = ref ('Shopping List App')
+  const items = ref([ 
+    {id: 1, label: "10 tomates"},
+    {id: 2, label: "5 chocolates"},
+    {id: 3, label: "2 gaseosas"},
+  ])
+
+  const newItem = ref("")
+  const newItemHightPriority = ref("low")
 </script>
 
 <template>
-  
+  <h1> {{ header }}</h1>
+
+  <form @submit.prevent="items.push({id: items.length + 1, label: newItem})">
+    <input v-model.trim="newItem" type="text" placeholder="Añadir producto"> {{ newItem }}
+    <label>
+      <input type="checkbox" v-model="newItemHightPriority">Muy importante
+    </label>
+    <button>Añadir a la lista</button>
+  </form>
+  <ul>
+    <li v-for="({id, label}, index) in items" :key="id">{{ label }}</li>
+  </ul>
 </template>
 
 <style lang="scss">
