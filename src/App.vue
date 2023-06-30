15bbb7ccd71ae01effba9f0e68335129439d8001
jgjorge96 <jgjorge96@gmail.com>
2023-06-30 11:59:42 -0300

    styling with sass ready

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <script setup>
   import { ref, computed } from 'vue';
 
-  const header = ref ('Shopping List App')
   const editing = ref(false)
   const items = ref([ 
-    {id: 1, label: "10 tomates", purchased: true, highPriority: false},
-    {id: 2, label: "5 chocolates", purchased: true, highPriority: false},
-    {id: 3, label: "2 gaseosas", purchased: false, highPriority: true},
+    // {id: 1, label: "10 tomates", purchased: true, highPriority: false},
+    // {id: 2, label: "5 chocolates", purchased: true, highPriority: false},
+    // {id: 3, label: "2 gaseosas", purchased: false, highPriority: true},
   ])
   const reversedItems = computed(()=> [...items.value].reverse() )
 
@@ -28,31 +27,28 @@
 </script>
 
 <template>
-  <div>
-    <h1> {{ header }}</h1>
-    <button v-if="editing" @click="doEdit(false)">Cancelar</button>
-    <button v-else @click="doEdit(true)">Añadir Producto</button>
+  <div class="header-container">
+    <h1> Shopping List App with Vue</h1>
+    <button v-if="editing" @click="doEdit(false)" class="btn-cancel">Cancelar</button>
+    <button v-else @click="doEdit(true)" class="btn-add">Añadir Producto</button>
   </div>
 
-  <form @submit.prevent="saveItems" v-if="editing">
-    <input v-model.trim="newItem" type="text" placeholder="Añadir producto">
-    <label>
-      <input type="checkbox" v-model="newItemHightPriority">Muy importante
-    </label>
-    <button :disabled="newItem.length < 2">Añadir a la lista</button>
-  </form>
-  <ul>
-    <li v-for="(item, index) in reversedItems" @click="togglePurchased(item)" :key="item.id" :class="{strikeout: item.purchased, priority: item.highPriority}">{{ item.label }}</li>
-  </ul>
-  <p v-if="!items.length">Lista vacia</p>
+  <div class="main-container">
+    <form @submit.prevent="saveItems" v-if="editing">
+      <input v-model.trim="newItem" type="text" placeholder="Añadir producto" class="text-input">
+      <label class="checkbox">
+        <input type="checkbox" v-model="newItemHightPriority">Muy importante
+      </label>
+      <button :disabled="newItem.length < 2">Añadir a la lista</button>
+    </form>
+    <ul>
+      <li v-for="(item, index) in reversedItems" @click="togglePurchased(item)" :key="item.id" :class="{priority: item.highPriority, strikeout: item.purchased}">{{ item.label }}</li>
+    </ul>
+    <p v-if="!items.length">Lista vacia</p>
+  </div>
 </template>
 
 <style lang="scss">
   @import './assets/styles.scss';
-  .strikeout {
-    color: blue;
-  }
-  .priority {
-    color: red;
-  }
+  
 </style>
